5f7a856bb926b74dba5364ea3b1a92f0c30e991f
ponganan <joeyubu@gmail.com>
2024-04-07 01:37:20 +0700

    add ToHtmlPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ToHtmlPipe } from './to-html.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ToHtmlPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/to-html.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { Document } from '@contentful/rich-text-types';
+
+@Pipe({
+  name: 'toHtml'
+})
+export class ToHtmlPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    //return document from richtext to string
+    return documentToHtmlString(value as Document);
+  }
+
+}
